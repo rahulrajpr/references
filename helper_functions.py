73aa1663781a14e2d18fd6fa530e991bfbb7ac4d
rahulrajml <88614222+rahulrajpr@users.noreply.github.com>
2023-02-16 11:49:41 +0530

    fixing the dependancies

--- helper_functions.py
+++ helper_functions.py
@@ -4,6 +4,7 @@
 import tensorflow as tf
 import os
 import random
+import matplotlib.image as mpimg
 
 # Create a function to import an image and resize it to be able to be used with our model
 def load_and_prep_image(filename, img_shape=224, scale=True):
